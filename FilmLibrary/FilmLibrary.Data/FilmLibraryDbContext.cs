9f60dd9be2e1be900cbb470c8ef6dc0859aed776
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-12-11 22:09:51 +0200

    Added data layer

---
+++ FilmLibrary/FilmLibrary.Data/FilmLibraryDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace FilmLibrary.Web.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    public class FilmLibraryDbContext : IdentityDbContext
+    {
+        public FilmLibraryDbContext(DbContextOptions<FilmLibraryDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
