f9c16ae59e46a7f02812650802ff053b1bee736c
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-12-12 21:44:01 +0200

    Added data constants

---
+++ FilmLibrary/FilmLibrary.Data/Common/DataConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace FilmLibrary.Data.Common
+{
+    public static class DataConstants
+    {
+        public const int GENRE_NAME_MAX_LENGHT = 50;
+
+        public const int MOVIE_TITLE_MAX_LENGHT = 50;
+        public const int DESCRIPTION_MAX_LENGHT = 1000;
+        public const int PRODUCTION_MAX_LENGHT = 70;
+        public const int ORIGINAL_LANGUAGE_MAX_LENGHT = 30;
+
+        public const int COUNTRY_NAME_MAX_LENGHT = 60;
+        public const int COUNTRY_CODE_MAX_LENGHT = 3;
+
+        public const int DIRECTOR_NAME_MAX_LENGHT = 40;
+    }
+}
