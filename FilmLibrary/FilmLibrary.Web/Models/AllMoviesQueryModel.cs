00d2441afcf2406a0de69eee6cef5a2209a169ae
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-12-15 20:50:07 +0200

    Added all movies view model

---
+++ FilmLibrary/FilmLibrary.Web/Models/AllMoviesQueryModel.cs
@@ -0,0 +1,22 @@
+﻿namespace FilmLibrary.Web.Models
+{
+    using FilmLibrary.Core.Services.Models;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class AllMoviesQueryModel
+    {
+        public const int MoviesPerPage = 3;
+
+        public string Title { get; init; }
+
+        [Display(Name = "Search by text")]
+        public string SearchTerm { get; init; }
+
+        public int CurrentPage { get; init; } = 1;
+
+        public int TotalMovies { get; set; }
+
+        public IEnumerable<MovieServiceModel> Movies { get; set; }
+    }
+}
