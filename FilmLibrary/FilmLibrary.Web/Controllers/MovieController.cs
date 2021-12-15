1c9d4aa9a6f3aab3da0a889b19c94b6e03827291
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-12-15 18:33:06 +0200

    Added movie controller

---
+++ FilmLibrary/FilmLibrary.Web/Controllers/MovieController.cs
@@ -0,0 +1,14 @@
+﻿namespace FilmLibrary.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
