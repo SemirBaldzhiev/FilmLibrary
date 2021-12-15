a900114a0591bd3517f4b085aa62532f533e28d5
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-12-15 18:30:35 +0200

    Added movie create and edit services

---
+++ FilmLibrary/FilmLibrary.Core/Services/Movies/IMovieService.cs
@@ -0,0 +1,32 @@
+﻿namespace FilmLibrary.Core.Services.Movies
+{
+    using System;
+
+    public interface IMovieService
+    {
+        int Create(string title,
+            string description,
+            int directorId,
+            DateTime releaseDate,
+            TimeSpan duration,
+            string production,
+            string originalLanguage,
+            string imageUrl,
+            int genreId,
+            int countryId);
+
+        bool Edit(int id,
+            string title,
+            string description,
+            int directorId,
+            DateTime releaseDate,
+            TimeSpan duration,
+            string production,
+            string originalLanguage,
+            string imageUrl,
+            int genreId,
+            int countryId);
+
+
+    }
+}
---
+++ FilmLibrary/FilmLibrary.Core/Services/Movies/MovieService.cs
@@ -0,0 +1,56 @@
+﻿using FilmLibrary.Data.Models;
+using FilmLibrary.Web.Data;
+using System;
+
+namespace FilmLibrary.Core.Services.Movies
+{
+    public class MovieService : IMovieService
+    {
+
+        private readonly FilmLibraryDbContext context;
+        public int Create(string title, string description, int directorId, DateTime releaseDate, TimeSpan duration, string production, string originalLanguage, string imageUrl, int genreId, int countryId)
+        {
+            var movieData = new Movie
+            {
+                Title = title,
+                Description = description,
+                DirectorId = directorId,
+                ReleaseDate = releaseDate,
+                Production = production,
+                OriginalLanguage = originalLanguage,
+                ImageUrl = imageUrl,
+                GenreId = genreId,
+                CountryId = countryId
+            };
+
+            this.context.Movies.Add(movieData);
+            this.context.SaveChanges();
+
+            return movieData.Id;
+        }
+
+        public bool Edit(int id, string title, string description, int directorId, DateTime releaseDate, TimeSpan duration, string production, string originalLanguage, string imageUrl, int genreId, int countryId)
+        {
+            var movie = this.context.Movies.Find(id);
+
+            if (movie == null)
+            {
+                return false;
+            }
+
+            movie.Title = title;
+            movie.Description = description;
+            movie.DirectorId = directorId;
+            movie.ReleaseDate = releaseDate;
+            movie.Production = production;
+            movie.OriginalLanguage = originalLanguage;
+            movie.ImageUrl = imageUrl;
+            movie.GenreId = genreId;
+            movie.CountryId = countryId;
+
+            this.context.SaveChanges();
+
+            return true;
+        }
+    }
+}
