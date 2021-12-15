8e9c75983122a9fdd0bbc0f817ea0f1ba4c9182c
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-12-15 20:48:36 +0200

    Added Index action

--- FilmLibrary/FilmLibrary.Web/Controllers/MovieController.cs
+++ FilmLibrary/FilmLibrary.Web/Controllers/MovieController.cs
@@ -1,12 +1,29 @@
 ﻿namespace FilmLibrary.Web.Controllers
 {
+    using FilmLibrary.Core.Services.Movies;
+    using FilmLibrary.Web.Models;
     using Microsoft.AspNetCore.Mvc;
 
     public class MovieController : Controller
     {
-        public IActionResult Index()
+        private readonly IMovieService movies;
+
+        public MovieController(IMovieService movies)
         {
-            return View();
+            this.movies = movies;
+        }
+        public IActionResult Index(AllMoviesQueryModel query)
+        {
+            var queryResult = this.movies.All(
+                query.Title,
+                query.SearchTerm,
+                query.CurrentPage,
+                AllMoviesQueryModel.MoviesPerPage);
+
+            query.TotalMovies = queryResult.TotalMovies;
+            query.Movies = queryResult.Movies;
+
+            return View(query);
         }
 
 
